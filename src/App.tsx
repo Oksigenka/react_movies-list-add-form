be39203b1cc95475443a2ea909d0b924e3dfd271
Oksigenka <ksushakarpenko635@gmail.com>
2025-07-03 15:42:57 +0200

    Add form for movies

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export const App = () => {
   const [movies, setMovies] = useState<Movie[]>(moviesFromServer);
 
   const addMovie = (newMovie: Movie) => {
-    setMovies(curruntMovie => [...curruntMovie, newMovie]);
+    setMovies(currentMovies => [...currentMovies, newMovie]);
   };
 
   return (
